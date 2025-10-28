263d379c0c5b3920976908275eaf7a5c73894b5a
Valli81106 <sreevalliiscute1234@gmail.com>
2025-10-28 16:55:28 +0530

    Made rough home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
 export default function About () {
     return (
         <>
-        <div className="bg-amber-900 text-4xl">
-            About Me
+        <div className="bg-[#0B685C] text-white">
+            <div>
+                <div className="flex justify-end">
+                    <div>
+                        {/* another tsar */}
+                    </div>
+
+                    <div className="text-5xl text-black text-[8px] font-bold mr-10">
+                        about me.
+                    </div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+            </div>
         </div>
         </>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,30 +6,34 @@ export default function Home () {
                     {/*3d character ka code */}
                 </div>
 
-                <div>
-                    <div>
-                        <div>
+                <div className="flex flex-col pt-40 pl-20">
+                    <div className="flex flex-row">
+                        <div className="text-[85px] font-bold ">
                             {/* hi im */}
                             Hi, I'm
                         </div>
 
-                        <div>
+                        <div className="ml-10">
                             {/* star element or image */}
+                            <img src="public\starblack.svg" alt="star" />
                         </div>
                     </div>
 
-                    <div>
+                    <div className="text-9xl font-extrabold text-[#E57375]">
                         {/* sreevalli*/}
                         sreevalli
                     </div>
 
-                    <div>
+                    <div className="pt-10 text-5xl ">
                         {/* your webdeveloper or smth*/}
                         your webdeveloper
                     </div>
 
-                    <div>
+                    <div className="pt-15 ml-30 items-center">
                         {/* button */}
+                        <button className=" pl-2 pr-2 pt-1 pb-1 text-black  text-lg bg-[#E57375] rounded-3xl font-semibold hover:bg-[#d86466] hover:cursor-pointer ">
+                            touch me
+                        </button>
                     </div>
                 </div>
             </div>
